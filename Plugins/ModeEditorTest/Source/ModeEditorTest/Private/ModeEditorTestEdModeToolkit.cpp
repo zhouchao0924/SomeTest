fdd101ae8455fb879e202f02fa668fd503ff5f36
Smartuil <563412673@qq.com>
2020-07-04 15:50:54 +0800

    移动中心点逻辑

--- Plugins/ModeEditorTest/Source/ModeEditorTest/Private/ModeEditorTestEdModeToolkit.cpp
+++ Plugins/ModeEditorTest/Source/ModeEditorTest/Private/ModeEditorTestEdModeToolkit.cpp
@@ -6,8 +6,26 @@
 #include "Widgets/Input/SButton.h"
 #include "Widgets/Text/STextBlock.h"
 #include "EditorModeManager.h"
+#include "ScopedTransaction.h"
+#include "Engine/Polys.h"
+#include "Model.h"
+#include "Components/BrushComponent.h"
+#include "Components/SkinnedMeshComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "UnrealEdGlobals.h"
+#include "Editor/UnrealEdEngine.h"
+#include "Widgets/Layout/SScrollBox.h"
+#include "Rendering/SkeletalMeshRenderData.h"
+#include "Rendering/SkeletalMeshLODRenderData.h"
 
 #define LOCTEXT_NAMESPACE "FModeEditorTestEdModeToolkit"
+#define COMPARE_LOCTEXT(TextA,TextB)\
+TextA.ToString() == TextB.ToString()
+
+namespace LocalNode
+{
+	static FText LeftFaceBehindTopID = LOCTEXT("LeftFaceBehindTopID", "Left Face Behind Top Point");
+}
 
 FModeEditorTestEdModeToolkit::FModeEditorTestEdModeToolkit()
 {
@@ -20,6 +38,167 @@ void FModeEditorTestEdModeToolkit::Init(const TSharedPtr<IToolkitHost>& InitTool
 		static bool IsWidgetEnabled()
 		{
 			return GEditor->GetSelectedActors()->Num() != 0;
+		}
+
+		static FReply OnButtonClickPointChange(FText InID)
+		{
+			if (GEditor->GetSelectedActorCount() <= 0)
+			{
+				return FReply::Unhandled();
+			}
+
+			const FScopedTransaction Transaction(LOCTEXT("ActorsPivotChange", "Actors Pivot Change"));
+
+			for (FSelectionIterator It(GEditor->GetSelectedActorIterator()); It; ++It)
+			{
+				AActor *MyActor = static_cast<AActor*>(*It);
+				FVector PivotPoint = GetSelectActorPivot(MyActor, InID);
+
+				FVector Delta(PivotPoint - MyActor->GetActorLocation());
+				PivotPoint = MyActor->GetTransform().InverseTransformVector(Delta);
+
+				if (MyActor->GetPivotOffset() != PivotPoint)
+				{
+					MyActor->Modify();
+					MyActor->SetPivotOffset(PivotPoint);
+				}
+			}
+
+			GUnrealEd->NoteSelectionChange();
+
+			return FReply::Handled();
+		}
+
+		static FVector GetSelectActorPivot(AActor *Actor, const FText &ID)
+		{
+			if (FMath::IsNearlyZero(Actor->GetComponentsBoundingBox().GetVolume(), KINDA_SMALL_NUMBER))
+			{
+				return Actor->GetActorLocation();
+			}
+
+			FVector Point(0);
+			const FTransform ActorTransform = Actor->GetActorTransform();
+
+			TArray<UPrimitiveComponent*> ValidComponents;
+			GetValidComponents(Actor, ValidComponents);
+
+			FBox MyBound(EForceInit::ForceInitToZero);
+			if (ValidComponents.Num() == 1 &&
+				Actor->GetRootComponent() == ValidComponents[0] &&
+				GetComponentBox(ValidComponents[0], MyBound))
+			{
+				FVector RelativePoint = GetMyBoundingBoxPoint(MyBound, ID);
+				Point = ActorTransform.TransformPosition(RelativePoint);
+			}
+
+			return Point;
+		}
+
+		static FVector GetMyBoundingBoxPoint(const FBox& InBoundingBox, FText ID)
+		{
+			FVector Result = FVector::ZeroVector;
+
+			FVector Origin = InBoundingBox.GetCenter();
+			FVector Extent = InBoundingBox.GetExtent();
+			FVector Offset = FVector::ZeroVector;
+
+			/////////////////////////////////////C/////////////////////////////////////
+			if (COMPARE_LOCTEXT(ID, LocalNode::LeftFaceBehindTopID))
+			{
+				Offset = FVector(0, 0, Extent.Z);
+			}
+
+			Result = Origin + Offset;
+
+			return Result;
+		}
+
+		static bool GetComponentBox(UPrimitiveComponent* InComponent, FBox& OutBox)
+		{
+			bool bBox = false;
+
+			if (InComponent)
+			{
+				if (!bBox)
+				{
+					const UStaticMeshComponent* StaticMeshComponent = Cast<const UStaticMeshComponent>(InComponent);
+					if (StaticMeshComponent != NULL && StaticMeshComponent->GetStaticMesh() != NULL)
+					{
+						OutBox = StaticMeshComponent->GetStaticMesh()->GetBoundingBox();
+						return true;
+					}
+				}
+
+				if (!bBox)
+				{
+					const UBrushComponent* BrushComponent = Cast<const UBrushComponent>(InComponent);
+					if (BrushComponent != NULL && BrushComponent->Brush != NULL)
+					{
+						OutBox = BrushComponent->Brush->Bounds.GetBox();
+						return true;
+					}
+				}
+
+				if (!bBox)
+				{
+					const USkinnedMeshComponent* SkinnedMeshComponent = Cast<const USkinnedMeshComponent>(InComponent);
+					if (SkinnedMeshComponent != NULL && SkinnedMeshComponent->SkeletalMesh != NULL)
+					{
+						OutBox = SkinnedMeshComponent->SkeletalMesh->GetImportedBounds().GetBox();
+						return true;
+					}
+				}
+			}
+
+			return bBox;
+		}
+
+		static void GetValidComponents(AActor* MyActor, TArray<UPrimitiveComponent*>& MyComponents)
+		{
+			if (MyActor)
+			{
+				TArray<UPrimitiveComponent*> PrimitiveComponents;
+				MyActor->GetComponents<UPrimitiveComponent>(PrimitiveComponents);
+
+				for (UPrimitiveComponent* Component : PrimitiveComponents)
+				{
+					bool bValidMesh = false;
+
+					if (!bValidMesh)
+					{
+						UStaticMeshComponent* StaticMeshComponent = Cast<UStaticMeshComponent>(Component);
+
+						bValidMesh = (
+							StaticMeshComponent &&
+							StaticMeshComponent->GetStaticMesh() &&
+							StaticMeshComponent->GetStaticMesh()->RenderData->LODResources[0].VertexBuffers.PositionVertexBuffer.GetNumVertices() > 0);
+					}
+
+					if (!bValidMesh)
+					{
+						UBrushComponent* BrushComponent = Cast<UBrushComponent>(Component);
+
+						bValidMesh = (
+							BrushComponent &&
+							BrushComponent->Brush &&
+							BrushComponent->Brush->Polys->Element.Num() > 0);
+					}
+
+					if (!bValidMesh)
+					{
+						USkinnedMeshComponent* SkinnedMeshComponent = Cast<USkinnedMeshComponent>(Component);
+
+						bValidMesh = (
+							SkinnedMeshComponent &&
+							SkinnedMeshComponent->GetSkeletalMeshRenderData()->LODRenderData[0].RenderSections[0].NumVertices > 0);
+					}
+
+					if (bValidMesh)
+					{
+						MyComponents.Add(Component);
+					}
+				}
+			}
 		}
 
 		static FReply OnButtonClick(FVector InOffset)
@@ -52,6 +231,13 @@ void FModeEditorTestEdModeToolkit::Init(const TSharedPtr<IToolkitHost>& InitTool
 			return SNew(SButton)
 				.Text(InLabel)
 				.OnClicked_Static(&Locals::OnButtonClick, InOffset);
+		}
+
+		static TSharedRef<SWidget> MakePointButton(FText InID)
+		{
+			return SNew(SButton)
+				.Text(InID)
+				.OnClicked_Static(&Locals::OnButtonClickPointChange, InID);
 		}
 	};
 
@@ -99,6 +285,12 @@ void FModeEditorTestEdModeToolkit::Init(const TSharedPtr<IToolkitHost>& InitTool
 				.AutoHeight()
 				[
 					Locals::MakeButton(LOCTEXT("DownButtonLabel", "Down"), FVector(0, 0, -Factor))
+				]
+			+ SVerticalBox::Slot()
+				.HAlign(HAlign_Center)
+				.AutoHeight()
+				[
+					Locals::MakePointButton(LocalNode::LeftFaceBehindTopID)
 				]
 
 		];
